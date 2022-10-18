4f5f361edd6409683310c2b65aae9ebba5334ac3
hyhello <673089899@qq.com>
2022-10-18 09:53:43 +0800

    feat: 编写核心插件

--- packages/cli/src/utils/loadPlugin.ts
+++ packages/cli/src/utils/loadPlugin.ts
@@ -1,5 +1,41 @@
 #! /usr/bin/env node
 
-export const loadPlugin = (pluginName?: string) => {
-	console.log(pluginName);
+import logger from './logger';
+import { resolveCWD } from './index';
+import { isArray } from '@hyhello/utils';
+
+const pluginKeywordReg = /^[@\w]+\/plugin-[\w-]+$/;
+
+// 加载模块
+function importModule(name: string) {
+	const mod = require(resolveCWD(`node_modules/${name}`));
+	return mod && mod.default ? mod.default : mod;
+}
+
+const loadPlugin = (plugins: any[] = []) => {
+	return plugins.reduce((pluginList, plugin) => {
+		let pluginName: string;
+		let pluginOpt: object = {};
+		if (typeof plugin === 'string') {
+			pluginName = plugin;
+		} else if (isArray(plugin)) {
+			pluginName = plugin[0];
+			pluginOpt = plugin[1] || {};
+		}
+		if (!pluginName) return pluginList;
+		// 补全 plugin 关键字
+		const moduleName = pluginKeywordReg.test(pluginName)
+			? pluginName
+			: pluginName?.replace(/\/([\w-]+)$/, '/plugin-$1');
+
+		try {
+			const PluginClass = importModule(moduleName);
+			pluginList.push(new PluginClass(pluginOpt));
+			return pluginList;
+		} catch (e) {
+			logger.error(`Plugin '${pluginName}' loading failed：${e}`);
+		}
+	}, []);
 };
+
+export default loadPlugin;
--- packages/plugin-email/lib/index.js
+++ packages/plugin-email/lib/index.js
@@ -1,26 +1,22 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-const tslib_1 = require("tslib");
-const nodemailer_1 = tslib_1.__importDefault(require("nodemailer"));
-function main() {
-    return tslib_1.__awaiter(this, void 0, void 0, function* () {
-        const transporter = nodemailer_1.default.createTransport({
-            service: 'qq',
-            secure: false,
-            auth: {
-                user: '673089899@qq.com',
-                pass: 'eapcuwtscgiwbddd'
-            }
+class PluginEmail {
+    constructor(opts = {}) {
+        this.opts = opts;
+    }
+    apply(compiler) {
+        compiler.hook.beforeExec.tap('PluginEmail', () => {
+            console.log('beforeExec');
         });
-        const info = yield transporter.sendMail({
-            from: '"Fred Foo 👻" <673089899@qq.com>',
-            to: '582016306@qq.com,673089899@qq.com',
-            subject: 'Hello ✔',
-            text: 'Hello world?',
-            html: '<b>Hello world?</b>'
+
+        compiler.hook.afterExec.tap('PluginEmail', () => {
+            console.log('afterExec');
         });
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer_1.default.getTestMessageUrl(info));
-    });
+
+        compiler.hook.done.tap('PluginEmail', (arg) => {
+            console.log('plugin:opts', this.opts);
+            console.log('测试arg', arg);
+        });
+    }
 }
-main().catch(console.error);
+exports.default = PluginEmail;
